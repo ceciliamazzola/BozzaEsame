36ae47fe648f06aa6cd3b1c20b410091925bb3e7
Filo1102 <f.toniolo11@gmail.com>
2025-07-01 13:53:47 +0000

    FIRST

---
+++ pages/7_💬_Contact.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+st.markdown("""
+<div style="background-color: #1f2b38; padding: 20px; border-radius: 10px; color: white; font-family: 'Arial', sans-serif;">
+    <h3 style="color: #f45208;">📌 Project Information</h3>
+    <p>
+        Questo progetto è stato sviluppato nell’ambito di un corso di laurea del 
+        <strong>Politecnico di Milano</strong>. I dati utilizzati provengono dai siti ufficiali della 
+        <strong>NBA</strong> e sono stati impiegati esclusivamente per finalità didattiche e di ricerca.
+    </p>
+    <p>
+        Di seguito sono riportati i contatti dei membri del team di sviluppo.
+    </p>
+        Cecilia Mazzola: cecilia.mazzola@mail.polimi.it
+    <p>
+        Filippo Toniolo: filippo.toniolo@mail.polimi.it
+    </p>
+</div>
+""", unsafe_allow_html=True)
